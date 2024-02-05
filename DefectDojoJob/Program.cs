6c995e3d83b67e3535df6f47eb2d216c10f5798c
LaylaRichard <lri@opinum.com>
2024-02-05 14:33:18 +0100

    first commit

---
+++ DefectDojoJob/Program.cs
@@ -0,0 +1,7 @@
+using DefectDojoJob;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
