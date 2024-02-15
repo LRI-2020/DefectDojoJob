5f25fbe8c01859d2544a5f7daf06166448c4abab
LaylaRichard <lri@opinum.com>
2024-02-15 12:24:35 +0100

    users processor tests

--- DefectDojoJob.Tests/Services.Tests/Processors.Tests/UsersProcessor.Tests.cs
+++ DefectDojoJob.Tests/Services.Tests/Processors.Tests/UsersProcessor.Tests.cs
@@ -1,4 +1,5 @@
-﻿using AutoFixture.Xunit2;
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
 using DefectDojoJob.Models.DefectDojo;
 using DefectDojoJob.Models.Processor.Errors;
 using DefectDojoJob.Services.Interfaces;
@@ -14,32 +15,128 @@ public class UsersProcessorTests
     [Theory]
     [AutoMoqData]
     public async Task WhenUserNotNull_ProcessUserAsyncReturnsUserDefectDojoId(string username,
-        [Frozen]Mock<IDefectDojoConnector> defectDojoConnectorMock,
+        [Frozen] Mock<IDefectDojoConnector> defectDojoConnectorMock,
         UsersProcessor sut)
     {
         //Arrange
         defectDojoConnectorMock.Setup(d => d.GetDefectDojoUserByUsername(username))
             .ReturnsAsync(new User() { Id = 1, UserName = username });
-        
+
         var res = await sut.ProcessUserAsync(username);
-        
+
         res.AssetIdentifier.Should().Be(username);
         res.DefectDojoId.Should().Be(1);
     }
-    
+
     [Theory]
     [AutoMoqData]
     public async Task WhenUserIsNull_ProcessUserAsyncThrowException(string username,
-        [Frozen]Mock<IDefectDojoConnector> defectDojoConnectorMock,
+        [Frozen] Mock<IDefectDojoConnector> defectDojoConnectorMock,
         UsersProcessor sut)
     {
         //Arrange
         defectDojoConnectorMock.Setup(d => d.GetDefectDojoUserByUsername(username))
             .ReturnsAsync((User?)null);
-        
-        Func<Task> act = ()=>sut.ProcessUserAsync(username);
+
+        Func<Task> act = () => sut.ProcessUserAsync(username);
 
         await act.Should().ThrowAsync<WarningAssetProjectInfoProcessor>()
             .Where(e => e.Message.Contains("user") && e.Message.Contains("does not exist"));
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenUserProcessingOk_UserFoundAddedToResultEntities(string username,
+        [Frozen] Mock<IDefectDojoConnector> defectDojoConnectorMock,
+        UsersProcessor sut)
+    {
+        //Arrange
+        defectDojoConnectorMock.Setup(d => d.GetDefectDojoUserByUsername(username))
+            .ReturnsAsync(new User() { Id = 1, UserName = username });
+
+        var usernames = new List<string>() { username };
+
+        var res = await sut.ProcessUsersAsync(usernames);
+
+        res.Entities.Count.Should().Be(1);
+        res.Entities[0].AssetIdentifier.Should().Be(username);
+        res.Entities[0].DefectDojoId.Should().Be(1);
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenWarning_WarningAddedToResult(string username,
+        [Frozen] Mock<IDefectDojoConnector> defectDojoConnectorMock,
+        UsersProcessor sut)
+    {
+        //Arrange
+        defectDojoConnectorMock.Setup(d => d.GetDefectDojoUserByUsername(username))
+            .ReturnsAsync((User?)null);
+        var usernames = new List<string>() { username };
+
+        var res = await sut.ProcessUsersAsync(usernames);
+
+        res.Entities.Should().BeEmpty();
+        res.Warnings.Count.Should().Be(1);
+        res.Warnings[0].AssetIdentifier.Should().Be(username);
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenError_ErrorAddedToResult(string username,
+        [Frozen] Mock<IDefectDojoConnector> defectDojoConnectorMock,
+        UsersProcessor sut)
+    {
+        //Arrange
+        defectDojoConnectorMock.Setup(d => d.GetDefectDojoUserByUsername(It.IsAny<string>()))
+            .Throws<Exception>();
+        var usernames = new List<string>() { username };
+
+        var res = await sut.ProcessUsersAsync(usernames);
+
+        res.Entities.Should().BeEmpty();
+        res.Errors.Count.Should().Be(1);
+        res.Errors[0].AssetIdentifier.Should().Be(username);
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenSeveralUsernames_AllAreProcessed(
+        [Frozen] Mock<IDefectDojoConnector> defectDojoConnectorMock,
+        UsersProcessor sut)
+    {
+        //Arrange
+        defectDojoConnectorMock.Setup(d => d.GetDefectDojoUserByUsername(It.IsAny<string>()))
+            .ReturnsAsync(new User() { Id = 1, UserName = "username" });
+
+        var itemsNumber = new Random().Next(3, 15);
+        var usernames = new Fixture().CreateMany<string>(itemsNumber).ToList();
+
+        var res = await sut.ProcessUsersAsync(usernames);
+
+        res.Entities.Count.Should().Be(itemsNumber);
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenErrorOccured_OtherEntitiesProcessed(string username1, string username2, string username3,
+        [Frozen] Mock<IDefectDojoConnector> defectDojoConnectorMock,
+        UsersProcessor sut)
+    {
+        //Arrange
+        defectDojoConnectorMock.Setup(d => d.GetDefectDojoUserByUsername(username3))
+            .ReturnsAsync(new User() { Id = 1, UserName = username1 });
+        defectDojoConnectorMock.Setup(d => d.GetDefectDojoUserByUsername(username2))
+            .ReturnsAsync((User?)null);
+        defectDojoConnectorMock.Setup(d => d.GetDefectDojoUserByUsername(username1))
+            .Throws<Exception>();
+
+        var usernames = new List<string>() { username1, username2, username3 };
+
+        var res = await sut.ProcessUsersAsync(usernames);
+
+        res.Entities.Count.Should().Be(1);
+        res.Warnings.Count.Should().Be(1);
+        res.Errors.Count.Should().Be(1);
     }
 }
--- DefectDojoJob/Models/Processor/Errors/WarningAssetProjectInfoProcessor.cs
+++ DefectDojoJob/Models/Processor/Errors/WarningAssetProjectInfoProcessor.cs
@@ -4,12 +4,11 @@ namespace DefectDojoJob.Models.Processor.Errors;
 
 public class WarningAssetProjectInfoProcessor : Exception
 {
-    private readonly EntityType entityType;
     public string AssetIdentifier { get; set; }
     public EntityType EntityType { get; set; }
     public WarningAssetProjectInfoProcessor(string? message, string assetIdentifier, EntityType entityType) : base(message)
     {
-        this.entityType = entityType;
+        EntityType = entityType;
         AssetIdentifier = assetIdentifier;
     }
 }
