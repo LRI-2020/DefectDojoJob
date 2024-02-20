f18910bccd34cbb6ef1f32da9d9aeab32a479c33
LaylaRichard <lri@opinum.com>
2024-02-20 13:16:55 +0100

    update product tests

--- DefectDojoJob.Tests/Services.Tests/DefectDojoConnector.Tests/UpdateProductAsync.Tests.cs
+++ DefectDojoJob.Tests/Services.Tests/DefectDojoConnector.Tests/UpdateProductAsync.Tests.cs
@@ -56,4 +56,63 @@ public class UpdateProductAsyncTests
         actualBody.Should().BeEquivalentTo(expected);
     }
 
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenUpdate_UriIsCorrect(IConfiguration configuration)
+    {
+        //Arrange
+        var res = new Product("test", "test") { Id = 1 };
+        var fakeHttpHandler = TestHelper.GetFakeHandler(HttpStatusCode.Accepted, JsonConvert.SerializeObject(res));
+        var httpClient = new HttpClient(fakeHttpHandler);
+        httpClient.BaseAddress = new Uri("https://test.be");
+        var sut = new DefectDojoJob.Services.DefectDojoConnector(configuration, httpClient);
+
+        //Act
+        await sut.UpdateProductAsync(res);
+
+        //Assert
+        //Assert
+        var expectedAbsolutePath = $"/products/{res.Id}";
+        var actualUri = fakeHttpHandler.RequestUrl ?? new Uri("");
+        actualUri.AbsolutePath.Should().BeEquivalentTo(expectedAbsolutePath);
+    }
+    
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenSuccessful_ReturnProductUpdated(IConfiguration configuration, Product res)
+    {
+        //Arrange
+
+        var fakeHttpHandler = TestHelper.GetFakeHandler(HttpStatusCode.Accepted, JsonConvert.SerializeObject(res));
+        var httpClient = new HttpClient(fakeHttpHandler);
+        httpClient.BaseAddress = new Uri("https://test.be");
+        var sut = new DefectDojoJob.Services.DefectDojoConnector(configuration, httpClient);
+
+        //Act
+        var actualRes = await sut.UpdateProductAsync(res);
+
+        //Assert
+        actualRes.Should().BeOfType(typeof(Product));
+    }
+    
+    [Theory]
+    [InlineAutoMoqData(404,"No product with Id")]
+    [InlineAutoMoqData(403,"Error while updating the Project")]
+    public async Task WhenStatusUnsuccessful_ErrorThrown(int statusCode, string error, IConfiguration configuration, Product res)
+    {
+        //Arrange
+        var fakeHttpHandler = TestHelper.GetFakeHandler((HttpStatusCode)statusCode, JsonConvert.SerializeObject(res));
+        var httpClient = new HttpClient(fakeHttpHandler);
+        httpClient.BaseAddress = new Uri("https://test.be");
+        var sut = new DefectDojoJob.Services.DefectDojoConnector(configuration, httpClient);
+
+        //Act
+        Func<Task> act = () => sut.UpdateProductAsync(res);
+
+        //Assert
+        await act.Should().ThrowAsync<Exception>()
+            .Where(e => e.Message.Contains(error));
+    }
+    
+   
 }
