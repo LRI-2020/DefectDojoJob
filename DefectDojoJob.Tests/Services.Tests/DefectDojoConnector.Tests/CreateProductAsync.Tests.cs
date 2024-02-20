1cd7275b8633ac4da2bd3069f2cf16f4b684ce35
LaylaRichard <lri@opinum.com>
2024-02-20 12:29:29 +0100

    create add test cases

--- DefectDojoJob.Tests/Services.Tests/DefectDojoConnector.Tests/CreateProductAsync.Tests.cs
+++ DefectDojoJob.Tests/Services.Tests/DefectDojoConnector.Tests/CreateProductAsync.Tests.cs
@@ -14,14 +14,35 @@ public class CreateProductAsyncTests
 {
     [Theory]
     [AutoMoqData]
-    public async Task WhenCreate_ArgumentsMappedCorrectlyToBody(IConfiguration configuration,string name,string description)
+    public async Task WhenCreate_UriIsCorrect(IConfiguration configuration)
     {
         //Arrange
         var res = new Product("test", "test") { Id = 1 };
         var fakeHttpHandler = TestHelper.GetFakeHandler(HttpStatusCode.Accepted, JsonConvert.SerializeObject(res));
         var httpClient = new HttpClient(fakeHttpHandler);
         httpClient.BaseAddress = new Uri("https://test.be");
-        var sut = new DefectDojoJob.Services.DefectDojoConnector(configuration,httpClient);
+        var sut = new DefectDojoJob.Services.DefectDojoConnector(configuration, httpClient);
+
+        //Act
+        await sut.CreateProductAsync(res);
+
+        //Assert
+        //Assert
+        var expectedAbsolutePath = "/products/";
+        var actualUri = fakeHttpHandler.RequestUrl ?? new Uri("");
+        actualUri.AbsolutePath.Should().BeEquivalentTo(expectedAbsolutePath);
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenCreate_ArgumentsMappedCorrectlyToBody(IConfiguration configuration, string name, string description)
+    {
+        //Arrange
+        var res = new Product("test", "test") { Id = 1 };
+        var fakeHttpHandler = TestHelper.GetFakeHandler(HttpStatusCode.Accepted, JsonConvert.SerializeObject(res));
+        var httpClient = new HttpClient(fakeHttpHandler);
+        httpClient.BaseAddress = new Uri("https://test.be");
+        var sut = new DefectDojoJob.Services.DefectDojoConnector(configuration, httpClient);
 
         var productToCreate = new Product(name, description)
         {
@@ -37,7 +58,7 @@ public class CreateProductAsyncTests
         await sut.CreateProductAsync(productToCreate);
 
         //Assert
-        var expectedBody =  new
+        var expectedBody = new
         {
             name,
             description,
@@ -49,9 +70,46 @@ public class CreateProductAsyncTests
             external_audience = true,
             lifecycle = Lifecycle.construction
         };
- 
-        var actualBody = JsonConvert.DeserializeObject(fakeHttpHandler.RequestBody??"");
+
+        var actualBody = JsonConvert.DeserializeObject(fakeHttpHandler.RequestBody ?? "");
         var expected = JObject.Parse(JsonConvert.SerializeObject(expectedBody));
         actualBody.Should().BeEquivalentTo(expected);
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenSuccessful_ReturnProductCreated(IConfiguration configuration, Product res)
+    {
+        //Arrange
+
+        var fakeHttpHandler = TestHelper.GetFakeHandler(HttpStatusCode.Accepted, JsonConvert.SerializeObject(res));
+        var httpClient = new HttpClient(fakeHttpHandler);
+        httpClient.BaseAddress = new Uri("https://test.be");
+        var sut = new DefectDojoJob.Services.DefectDojoConnector(configuration, httpClient);
+
+        //Act
+        var actualRes = await sut.CreateProductAsync(res);
+
+        //Assert
+        actualRes.Should().BeOfType(typeof(Product));
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenStatusUnsuccessful_ErrorThrown(IConfiguration configuration, Product res)
+    {
+        //Arrange
+        var fakeHttpHandler = TestHelper.GetFakeHandler(HttpStatusCode.Forbidden, JsonConvert.SerializeObject(res));
+        var httpClient = new HttpClient(fakeHttpHandler);
+        httpClient.BaseAddress = new Uri("https://test.be");
+        var sut = new DefectDojoJob.Services.DefectDojoConnector(configuration, httpClient);
+
+        //Act
+        Func<Task> act = () => sut.CreateProductAsync(res);
+
+        //Assert
+        await act.Should().ThrowAsync<Exception>()
+            .Where(e => e.Message.Contains("Error while creating the Project")
+                        && e.Message.Contains(HttpStatusCode.Forbidden.ToString()));
     }
 }
---
+++ DefectDojoJob.Tests/Services.Tests/DefectDojoConnector.Tests/UpdateProductAsync.Tests.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using DefectDojoJob.Models.DefectDojo;
+using DefectDojoJob.Services;
+using DefectDojoJob.Tests.AutoDataAttribute;
+using DefectDojoJob.Tests.Helpers.Tests;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DefectDojoJob.Tests.Services.Tests.DefectDojoConnector.Tests;
+
+public class UpdateProductAsyncTests
+{
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenUpdate_ArgumentsMappedCorrectlyToBody(IConfiguration configuration,string name,string description)
+    {
+        //Arrange
+        var res = new Product("test", "test") { Id = 1 };
+        var fakeHttpHandler = TestHelper.GetFakeHandler(HttpStatusCode.Accepted, JsonConvert.SerializeObject(res));
+        var httpClient = new HttpClient(fakeHttpHandler);
+        httpClient.BaseAddress = new Uri("https://test.be");
+        var sut = new DefectDojoJob.Services.DefectDojoConnector(configuration,httpClient);
+
+        var productToUpdate = new Product(name, description)
+        {
+            Id=1,
+            ProductTypeId = 1,
+            Lifecycle = Lifecycle.construction,
+            TechnicalContact = 2,
+            TeamManager = 3,
+            ProductManager = 4,
+            UserRecords = 5,
+            ExternalAudience = true
+        };
+        //Act
+        await sut.UpdateProductAsync(productToUpdate);
+
+        //Assert
+        var expectedBody =  new
+        {
+            name,
+            description,
+            prod_type = 1,
+            technical_contact = 2,
+            team_manager = 3,
+            product_manager = 4,
+            user_records = 5,
+            external_audience = true,
+            lifecycle = Lifecycle.construction
+        };
+ 
+        var actualBody = JsonConvert.DeserializeObject(fakeHttpHandler.RequestBody??"");
+        var expected = JObject.Parse(JsonConvert.SerializeObject(expectedBody));
+        actualBody.Should().BeEquivalentTo(expected);
+    }
+
+}
