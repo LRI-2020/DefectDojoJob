1e7d1f31e2e1acc139727ab4ef4ab29f26af699a
LaylaRichard <lri@opinum.com>
2024-02-23 16:20:52 +0100

    Delete metadata + tests

--- DefectDojoJob.Tests/Services.Tests/DefectDojoConnectorTests/DeleteMetadataAsyncTests.cs
+++ DefectDojoJob.Tests/Services.Tests/DefectDojoConnectorTests/DeleteMetadataAsyncTests.cs
@@ -1,10 +1,62 @@
-﻿namespace DefectDojoJob.Tests.Services.Tests.DefectDojoConnectorTests;
+﻿using System.Net;
+using DefectDojoJob.Tests.Helpers.Tests;
+using Newtonsoft.Json;
+
+namespace DefectDojoJob.Tests.Services.Tests.DefectDojoConnectorTests;
 
 public class DeleteMetadataAsyncTests
 {
-    [Fact]
-    public void test()
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenDeleteMetadata_URiIsCorrect(IConfiguration configuration, int id)
     {
-        throw new NotImplementedException();
+        //Arrange
+        var fakeHttpHandler = TestHelper.GetFakeHandler(HttpStatusCode.Accepted);
+        var httpClient = new HttpClient(fakeHttpHandler);
+        httpClient.BaseAddress = new Uri("https://test.be");
+        var sut = new DefectDojoJob.Services.Connectors.DefectDojoConnector(configuration, httpClient);
+
+        //Act
+        await sut.DeleteMetadataAsync(id);
+
+        //Assert
+        var expectedAbsolutePath = $"/metadata/{id}";
+
+        var actualUri = fakeHttpHandler.RequestUrl ?? new Uri("");
+        actualUri.AbsolutePath.Should().BeEquivalentTo(expectedAbsolutePath);
+    }
+    
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenDeleteOk_TrueReturned(IConfiguration configuration, int id)
+    {
+        //Arrange
+        var fakeHttpHandler = TestHelper.GetFakeHandler(HttpStatusCode.Accepted);
+        var httpClient = new HttpClient(fakeHttpHandler);
+        httpClient.BaseAddress = new Uri("https://test.be");
+        var sut = new DefectDojoJob.Services.Connectors.DefectDojoConnector(configuration, httpClient);
+
+        //Act
+        var res = await sut.DeleteMetadataAsync(id);
+
+        //Assert
+        res.Should().BeTrue();
+    }
+    
+    [Theory]
+    [AutoMoqData]
+    public async Task WhenDeleteFailed_FalseReturned(IConfiguration configuration, int id)
+    {
+        //Arrange
+        var fakeHttpHandler = TestHelper.GetFakeHandler(HttpStatusCode.Forbidden);
+        var httpClient = new HttpClient(fakeHttpHandler);
+        httpClient.BaseAddress = new Uri("https://test.be");
+        var sut = new DefectDojoJob.Services.Connectors.DefectDojoConnector(configuration, httpClient);
+
+        //Act
+        var res = await sut.DeleteMetadataAsync(id);
+
+        //Assert
+        res.Should().BeFalse();
     }
 }
